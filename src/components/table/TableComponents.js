5986a783178d2c445d2d885369279e0c5ec77016
jugalshah14 <145976665+jugalshah14@users.noreply.github.com>
2023-11-22 15:44:27 +0530

    lkdfm

--- src/components/table/TableComponents.js
+++ src/components/table/TableComponents.js
@@ -7,21 +7,53 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import { useNavigate } from "react-router-dom";
 import "./TableStyle.css";
 
 const TableComponents = () => {
   const [tableData, setTableData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
+  const updateTableData = () => {
     const storedData = JSON.parse(localStorage.getItem("formData")) || [];
     setTableData(storedData);
-  }, []);
+    setFilteredData(storedData);
+  };
+
+  useEffect(() => {
+    updateTableData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [localStorage.getItem("formData")]);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      filterTableData();
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchTerm, tableData]);
+
+  const filterTableData = () => {
+    if (searchTerm === "") {
+      setFilteredData(tableData);
+    } else {
+      const filtered = tableData.filter((item) =>
+        Object.values(item).some((value) =>
+          String(value).toLowerCase().includes(searchTerm.toLowerCase())
+        )
+      );
+      setFilteredData(filtered);
+    }
+  };
 
   const handleDelete = (id) => {
     const updatedTableData = tableData.filter((item) => item.id !== id);
     setTableData(updatedTableData);
+    setFilteredData(updatedTableData);
     localStorage.setItem("formData", JSON.stringify(updatedTableData));
   };
 
@@ -31,6 +63,13 @@ const TableComponents = () => {
 
   return (
     <div className="table-body">
+      <div className="search">
+        <TextField
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <TableContainer component={Paper} className="table-container">
         <Table>
           <TableHead>
@@ -45,7 +84,7 @@ const TableComponents = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {tableData.map((item) => (
+            {filteredData.map((item) => (
               <TableRow key={item.id}>
                 <TableCell>{item.firstName}</TableCell>
                 <TableCell>{item.lastName}</TableCell>
